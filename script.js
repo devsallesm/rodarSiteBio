55ec1391d94fb191cb25d9ca74b95b1738d12d9e
miguel salles <miguel.absalles@gmail.com>
2025-05-29 09:59:21 -0300

    estrutura basica

---
+++ script.js
@@ -0,0 +1,54 @@
+perguntas = [ {
+    pergunta1 : "", 
+    opcoes : [],
+    resposta : 1
+},
+{
+    pergunta2 : "", 
+    opcoes : [],
+    resposta : 1
+},
+{
+    pergunta3 : "", 
+    opcoes : [],
+    resposta : 1
+},
+{
+    pergunta4 : "", 
+    opcoes : [],
+    resposta : 1
+},
+{
+    pergunta5 : "", 
+    opcoes : [],
+    resposta : 1
+},
+{
+    pergunta6 : "", 
+    opcoes : [],
+    resposta : 1
+},
+{
+    pergunta7 : "", 
+    opcoes : [],
+    resposta : 11
+},
+{
+    pergunta8 : "", 
+    opcoes : [],
+    resposta : 1
+},
+{
+    pergunta9 : "", 
+    opcoes : [],
+    resposta : 1
+},
+{
+    pergunta1 : "", 
+    opcoes : [],
+    resposta : 1
+}
+]
+
+fetch
+quiz = JSON.stringify(perguntas)
